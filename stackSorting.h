6b7670a89eaf575e4c2735676ab5a6977aea0632
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-02-27 15:31:48 +0100

    Adds the prototypes for future functions

--- stackSorting.h
+++ stackSorting.h
@@ -14,4 +14,21 @@
 #ifndef STACK_SORTING_H
 #define STACK_SORTING_H
 
+#define MAX_ARRAY_LENGTH 255
+
+/*
+***************************** Static variables *************************
+*/
+static int stackArray[MAX_ARRAY_LENGTH];
+
+/*
+***************************** Functions *************************
+*/
+void createStack(int* array, int arrayLength);
+void destroyStack(int* array, int arrayLength);
+
+int getLeftLeaf(int* array, int arrayLength, int currentIndex);
+int getRightLeaf(int* array, int arrayLength, int currentIndex);
+int getFather(int* array, int arrayLength, int currentIndex);
+
 #endif
