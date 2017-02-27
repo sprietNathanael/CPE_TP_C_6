189e9835b8338d2cd47a30a4c8d7b2be33d52eb6
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-02-27 15:12:49 +0100

    Adds a function to print an array

--- main.c
+++ main.c
@@ -10,7 +10,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "log.h"
+#define ARRAY_LENGTH 17
 
+
+void printArray(int* arrayToPrint, int arrayLength);
 
 int main()
 {
@@ -20,5 +23,18 @@ int main()
 	log_debug("This is a debug log\n");
 	log_false("This is a false log\n");
 	log_warning("This is a warning log\n");
+	int arrayToSort[] = {3 , 5 , 6 , 2 , 8 , 9 , 11 , 7 , 4 , 10 , 12 , 15 , 20 , 1 , 19 , 13 , 17};
+	printArray(arrayToSort, ARRAY_LENGTH);
 	return 0;
+}
+
+void printArray(int* arrayToPrint, int arrayLength)
+{
+	int i = 0;
+	log_info("Array : [");
+	for(i = 0; i < arrayLength; i++)
+	{
+		log_info(" %d ",arrayToPrint[i]);
+	}
+	log_info("]\n");
 }
